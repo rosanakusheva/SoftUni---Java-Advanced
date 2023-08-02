ad37b07a54515354736bf67b80344482c7cc52cf
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-08-02 19:13:03 +0300

    Stacks and Queues LAB
    Solution to the 'Matching Brackets' task

---
+++ MODULE1/src/StacksAndQueues/LAB/MatchingBrackets.java
@@ -0,0 +1,25 @@
+package StacksAndQueues.LAB;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        ArrayDeque<Integer> indexes = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                indexes.push(i);
+            } else if (input.charAt(i) == ')') {
+                int openBracketIndex = indexes.pop();
+
+                String expression = input.substring(openBracketIndex, i + 1);
+                System.out.println(expression);
+            }
+        }
+    }
+}
