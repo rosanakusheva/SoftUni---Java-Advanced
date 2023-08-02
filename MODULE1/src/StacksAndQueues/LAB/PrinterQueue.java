46c85f00259b52e76cd0f2f127523996b7946832
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-08-02 19:48:00 +0300

    Stacks and Queues LAB
    Solution to the 'Printer Queue' taks

---
+++ MODULE1/src/StacksAndQueues/LAB/PrinterQueue.java
@@ -0,0 +1,31 @@
+package StacksAndQueues.LAB;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> printerQueue = new ArrayDeque<>();
+        String input = scanner.nextLine();
+
+        while (!input.equals("print")) {
+            if (input.equals("cancel")) {
+                if (printerQueue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled " + printerQueue.poll());
+                }
+
+            } else {
+                printerQueue.offer(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        while (!printerQueue.isEmpty()) {
+            System.out.println(printerQueue.poll());
+        }
+    }
+}
