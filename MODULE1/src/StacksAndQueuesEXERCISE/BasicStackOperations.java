ddb689cf2bf84b3cd0eae62768ffb169ea150bfc
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-08-04 17:55:33 +0300

    Stacks and Queues EXERCISE
    Solution to the 'Basic Stack Operations' task

---
+++ MODULE1/src/StacksAndQueuesEXERCISE/BasicStackOperations.java
@@ -0,0 +1,33 @@
+package StacksAndQueuesEXERCISE;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 1; i <= n; i++) {
+            stack.push(scanner.nextInt());
+        }
+
+        for (int i = 1; i <= s; i++) {
+            stack.pop();
+        }
+        if (stack.contains(x)) {
+            System.out.println("true");
+        } else {
+            if (stack.isEmpty()) {
+                System.out.println(0);
+            } else {
+                System.out.println(Collections.min(stack));
+            }
+        }
+    }
+}
