a57fffe2dcb41fdaefa7da71dbee9ce9ce2d2931
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-08-20 10:58:40 +0300

    Sets and Maps EXERCISE
    Solution to the 'Hands of Cards' task

---
+++ MODULE1/src/SetsAndMapsEXERCISE/HandsOfCards.java
@@ -0,0 +1,78 @@
+package SetsAndMapsEXERCISE;
+
+import java.util.*;
+
+public class HandsOfCards {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Set<String>> players = new LinkedHashMap<>();
+        String input = scanner.nextLine();
+
+        while (!input.equals("JOKER")) {
+            String personName = input.split(":\\s+")[0];
+
+            String cards = input.split(":\\s+")[1];
+            String[] cardsArr = cards.split(", ");
+            Set<String> cardsSet = new HashSet<>();
+            cardsSet.addAll(Arrays.asList(cardsArr));
+
+            if (!players.containsKey(personName)) {
+                players.put(personName, cardsSet);
+            } else {
+                Set<String> currentCards = players.get(personName);
+                currentCards.addAll(cardsSet);
+                players.put(personName, currentCards);
+            }
+
+            input = scanner.nextLine();
+
+        }
+        players.entrySet().forEach(entry -> {
+            String name = entry.getKey();
+            Set<String> cards = entry.getValue();
+            int points = getCardsPoints(cards);
+            System.out.printf("%s: %d%n", name, points);
+        });
+    }
+
+    private static int getCardsPoints(Set<String> cards) {
+        Map<Character, Integer> symbolsValues = getSymbolsValues();
+        int sum = 0;
+        for (String card : cards) {
+            int points = 0;
+
+            if (card.startsWith("10")) {
+                char type = card.charAt(2);
+                points = 10 * symbolsValues.get(type);
+            } else {
+                char power = card.charAt(0);
+                char type = card.charAt(1);
+                points = symbolsValues.get(power) * symbolsValues.get(type);
+            }
+            sum += points;
+        }
+        return sum;
+    }
+
+    private static Map<Character, Integer> getSymbolsValues() {
+        Map<Character, Integer> characterValues = new HashMap<>();
+        characterValues.put('2', 2);
+        characterValues.put('3', 3);
+        characterValues.put('4', 4);
+        characterValues.put('5', 5);
+        characterValues.put('6', 6);
+        characterValues.put('7', 7);
+        characterValues.put('8', 8);
+        characterValues.put('9', 9);
+        characterValues.put('J', 11);
+        characterValues.put('Q', 12);
+        characterValues.put('K', 13);
+        characterValues.put('A', 14);
+        characterValues.put('S', 4);
+        characterValues.put('H', 3);
+        characterValues.put('D', 2);
+        characterValues.put('C', 1);
+        return characterValues;
+    }
+}
+
