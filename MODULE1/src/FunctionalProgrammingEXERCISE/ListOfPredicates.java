d73023733e651d91476b379309ad3e790fc653c7
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-09-01 14:57:26 +0300

    Functional Programming EXERCISE
    Solution to the 'List of Predicates' task

---
+++ MODULE1/src/FunctionalProgrammingEXERCISE/ListOfPredicates.java
@@ -0,0 +1,33 @@
+package FunctionalProgrammingEXERCISE;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+public class ListOfPredicates {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        BiFunction<Integer,List<Integer>, Boolean> isDivisible = ((number, list) -> {
+            for (int numInList : list){
+                if (number % numInList != 0){
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        for (int i = 1; i <= n ; i++) {
+               if (isDivisible.apply(i, numbers)){
+                   System.out.print(i + " ");
+               }
+        }
+    }
+}
