6cf6506129d3a701ed52872788e2782f51d54b39
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-10-13 16:45:15 +0300

    Working with abstraction EXERCISE
    Solution to the 'Card Ranks' task

---
+++ WorkingWithAbstraction/src/CardRank/Main.java
@@ -0,0 +1,15 @@
+package CardRank;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Ranks:");
+        CardRanks[] cardRanks = CardRanks.values();
+
+        for (CardRanks cardRank : cardRanks) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cardRank.ordinal(), cardRank);
+
+        }
+    }
+}
