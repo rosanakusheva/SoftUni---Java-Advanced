782728f41429cf25251e28c7ef742d8a3c83e000
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-10-15 19:37:27 +0300

    Encapsulation LAB
    added validation in the constructor

--- Encapsulation/src/SortByNameAndAge/Person.java
+++ Encapsulation/src/SortByNameAndAge/Person.java
@@ -6,12 +6,11 @@ public class Person {
     private int age;
     private double salary;
 
-    public Person(String firstName, String lastName, int age, double
-            salary) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.salary = salary;
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.setSalary(salary);
+        this.setAge(age);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
     }
 
     public String getFirstName() {
@@ -31,7 +30,31 @@ public class Person {
     }
 
     public void setSalary(double salary) {
+        if (salary < 460){
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
         this.salary = salary;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 3){
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length() < 3){
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age < 1){
+            throw  new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
     }
 
     public void increaseSalary(double percentage) {
