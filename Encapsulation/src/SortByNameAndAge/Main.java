1b1708ef5a757805d33a38f6fdd9468e0797b9bd
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-10-15 18:59:15 +0300

    Encapsulation LAB
    Solution to the 'Sort By Name and Age' task

---
+++ Encapsulation/src/SortByNameAndAge/Main.java
@@ -0,0 +1,42 @@
+package SortByNameAndAge;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            people.add(new Person(firstName, lastName, age));
+        }
+
+        Collections.sort(people, (firstPerson, secondPerson) -> {
+            int comparisonResult = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+
+//            if (comparisonResult == 0){
+//                comparisonResult = Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+//            }
+//            return comparisonResult;
+
+            if (comparisonResult != 0) {
+                return comparisonResult;
+            } else {
+                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+            }
+        });
+
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
+
+    }
+}
