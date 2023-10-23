06628f8aac377a28d410be8e9c0a40f8421f3f69
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-10-23 20:12:06 +0300

    Inheritance LAB
    Solution to the 'Hierarchical Inheritance' task

--- Inheritance/src/SingleInheritance/Main.java
+++ Inheritance/src/SingleInheritance/Main.java
@@ -2,8 +2,12 @@ package SingleInheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
+       Dog dog  = new Dog();
+       dog.eat();
+       dog.bark();
+
+       Cat cat = new Cat();
+       cat.eat();
+       cat.meow();
     }
 }
