09f9a7dbdc3bc0c9d7892645e04b17ffa8d288bb
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-10-23 20:40:01 +0300

    Inheritance LAB
    Solution to the 'Random Array List' task

---
+++ Inheritance/src/RandomArrayList/Main.java
@@ -0,0 +1,9 @@
+package RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList randomArrayList = new RandomArrayList();
+        randomArrayList.getRandomElement();
+    }
+
+}
---
+++ Inheritance/src/RandomArrayList/RandomArrayList.java
@@ -0,0 +1,12 @@
+package RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList {
+    public Object getRandomElement(){
+        int index = new Random().nextInt(super.size());
+        return super.remove(index);
+    }
+}
