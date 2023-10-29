791245c4a31eb899980ee181879a2b7c2fc8b54e
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-10-29 18:01:28 +0200

    Interfaces and Abstraction EXERCISE
    new logic added

--- InterfacesAndAbstraction/src/DefineAnInterfacePerson/Citizen.java
+++ InterfacesAndAbstraction/src/DefineAnInterfacePerson/Citizen.java
@@ -1,12 +1,16 @@
 package DefineAnInterfacePerson;
 
-public class Citizen implements Person{
+public class Citizen implements Person, Identifiable, Birthable{
     private String name;
     private int age;
+    private String id;
+    private String birthDate;
 
-    public Citizen(String name, int age) {
+    public Citizen(String name, int age, String id, String birthDate) {
         this.name = name;
         this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
     }
 
     @Override
@@ -17,5 +21,15 @@ public class Citizen implements Person{
     @Override
     public int getAge() {
         return this.age;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
     }
 }
---
+++ InterfacesAndAbstraction/src/DefineAnInterfacePerson/Identifiable.java
@@ -0,0 +1,7 @@
+package DefineAnInterfacePerson;
+
+public interface Identifiable {
+    String getId();
+
+
+}
