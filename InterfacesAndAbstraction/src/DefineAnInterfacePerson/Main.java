a29fe2929b389f99f7218ab0c33acb70e63d6511
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-10-30 18:25:15 +0200

    Interfaces and Abstraction EXERCISE
    new logic added

--- InterfacesAndAbstraction/src/DefineAnInterfacePerson/Main.java
+++ InterfacesAndAbstraction/src/DefineAnInterfacePerson/Main.java
@@ -1,32 +1,53 @@
 package DefineAnInterfacePerson;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Class[] citizenInterfaces = Citizen.class.getInterfaces();
-        if (Arrays.asList(citizenInterfaces).contains(Birthable.class)
-                && Arrays.asList(citizenInterfaces).contains(Identifiable.class)) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-            Method[] methods = Birthable.class.getDeclaredMethods();
-            Method[] methods1 = Identifiable.class.getDeclaredMethods();
+        List<Birthable> thingsWithBirthday = new ArrayList<>();
 
-            Scanner scanner = new Scanner(System.in);
-            String name = scanner.nextLine();
 
-            int age = Integer.parseInt(scanner.nextLine());
-            String id = scanner.nextLine();
-            String birthDate = scanner.nextLine();
+        while (!input.equals("End")) {
+            String[] inputParts = input.split("\\s+");
+            String typeToCreate = inputParts[0];
+            switch (typeToCreate) {
+                case "Citizen":
+                    String citizenName = inputParts[1];
+                    int age = Integer.parseInt(inputParts[2]);
+                    String id = inputParts[3];
+                    String citizenBirthDate = inputParts[3];
+                    Citizen citizen = new Citizen(citizenName, age, id, citizenBirthDate);
+                    thingsWithBirthday.add(citizen);
 
-            Identifiable identifiable = new Citizen(name,age,id,birthDate);
-            Birthable birthable = new Citizen(name,age,id,birthDate);
+                    break;
+                case "Pet":
+                    String petName = inputParts[1];
+                    String birthDate = inputParts[2];
+                    Pet pet = new Pet(petName, birthDate);
+                    thingsWithBirthday.add(pet);
 
-            System.out.println(methods.length);
-            System.out.println(methods[0].getReturnType().getSimpleName());
-            System.out.println(methods1.length);
-            System.out.println(methods1[0].getReturnType().getSimpleName());
+                    break;
+                case "Robot":
+
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+        String year = scanner.nextLine();
+
+        for (Birthable birthable : thingsWithBirthday) {
+            if (birthable.getBirthDate().endsWith(year)) {
+                System.out.println(birthable.getBirthDate());
+            }
         }
 
     }
