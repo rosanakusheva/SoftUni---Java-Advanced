ab9a6e55660e752e60e92edf509e3119bfe6e2c0
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-11-01 12:01:55 +0200

    Polymorphism LAB
    Solution to the 'Math Operation' task

---
+++ Polymorphism/src/MathOperation/Main.java
@@ -0,0 +1,9 @@
+package MathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperation mathOperation = new MathOperation();
+        mathOperation.add(1, 2);
+        mathOperation.add(1, 2, 3);
+    }
+}
